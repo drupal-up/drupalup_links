6be4622e113012c9113329fe6702233a5fee17c4
flesheater <nikolay.r.borisov@gmail.com>
2018-10-01 22:45:54 +0200

    Initial commit.

---
+++ src/Controller/DrupalUpLinksController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Drupal\drupalup_links\Controller;
+
+/**
+ * Example controller that we are going to use for tabs.
+ */
+class DrupalUpLinksController {
+
+  /**
+   * Main link page.
+   */
+  public function mainLinkPage() {
+    return [
+      '#markup' => 'Main link page.',
+    ];
+  }
+
+
+}
